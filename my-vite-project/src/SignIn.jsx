703cf792f100d3fba91c3974a1fd150918640c82
greengoee <147093073+greengoee@users.noreply.github.com>
2024-11-14 21:48:48 +0200

    Update SignIn.jsx

--- my-vite-project/src/SignIn.jsx
+++ my-vite-project/src/SignIn.jsx
@@ -20,7 +20,7 @@ const SignIn = ({ onSignIn, goToLogin }) => {
     }
 
     try {
-      const response = await fetch('https://reekelelitsoe-backend.onrender.com/signup', {
+      const response = await fetch('https://reekelelitsoe-backend.onrender.com:5000/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
